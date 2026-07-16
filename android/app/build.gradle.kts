45f80e1e6226621ec7d6f6faadcf2288edf95b73
alshiek1828 <alshiek1828@github.com>
2026-07-16 12:44:09 +0000

    Fix JVM target mismatch: set Java 17 and Kotlin toolchain 17

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,30 @@
 plugins { id("com.android.application"); id("kotlin-android"); id("dev.flutter.flutter-gradle-plugin") }
-android { namespace = "com.cashier.lebanon.pro"; compileSdk = flutter.compileSdkVersion; ndkVersion = flutter.ndkVersion
- defaultConfig { applicationId = "com.cashier.lebanon.pro"; minSdk = 24; targetSdk = flutter.targetSdkVersion; versionCode = flutter.versionCode; versionName = flutter.versionName }
- buildTypes { release { signingConfig = signingConfigs.getByName("debug") } }
+
+kotlin {
+    jvmToolchain(17)
 }
+
+android {
+    namespace = "com.cashier.lebanon.pro"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    defaultConfig {
+        applicationId = "com.cashier.lebanon.pro"
+        minSdk = 24
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release { signingConfig = signingConfigs.getByName("debug") }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
 flutter { source = "../.." }

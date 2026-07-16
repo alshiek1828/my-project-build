bc6fe22869bdb51d4bd297846dcb27ae7a8e2a70
alshiek1828 <alshiek1828@github.com>
2026-07-16 12:36:07 +0000

    Bump minSdk to 24 for image_picker_android compatibility

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins { id("com.android.application"); id("kotlin-android"); id("dev.flutter.flutter-gradle-plugin") }
 android { namespace = "com.cashier.lebanon.pro"; compileSdk = flutter.compileSdkVersion; ndkVersion = flutter.ndkVersion
- defaultConfig { applicationId = "com.cashier.lebanon.pro"; minSdk = 23; targetSdk = flutter.targetSdkVersion; versionCode = flutter.versionCode; versionName = flutter.versionName }
+ defaultConfig { applicationId = "com.cashier.lebanon.pro"; minSdk = 24; targetSdk = flutter.targetSdkVersion; versionCode = flutter.versionCode; versionName = flutter.versionName }
  buildTypes { release { signingConfig = signingConfigs.getByName("debug") } }
 }
 flutter { source = "../.." }
